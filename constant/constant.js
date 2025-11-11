2d878a83178dc5c2c1006baba4310014275815c9
shoaib mansoori <shoaibmansoori556@gmail.com>
2025-11-11 22:58:58 +0530

    Dynamic Error Messaage

--- constant/constant.js
+++ constant/constant.js
@@ -6,3 +6,16 @@ export const HTTP_STATUS_CODE = {
   Internal_Server_Error : 500,
   Forbidden : 403
 }
+
+export const MESSAGE = {
+  //User Message
+  User_Not_found : 'User Not Found',
+  Invalid_Email: 'Invalid email or password',
+  Email_Already_Exist : 'Email already exists',
+  Loggout_Message : 'Logged out successfully',
+  Refferesh_token_exp : 'Refresh token is required',
+   Invalid_Data : 'Invalid request data',
+
+  //question Message
+  Question_Not_Found: 'Question not found',
+}
